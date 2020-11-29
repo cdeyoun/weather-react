fee67e3ecc8e268bb9771227c01cde5a38b35a55
cdeyoun <69175805+cdeyoun@users.noreply.github.com>
2020-11-28 21:03:45 -0500

    Basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,17 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control"></input>
+                        <input 
+                        type="search" 
+                        placeholder="Enter a city..." 
+                        className="form-control"
+                        autoFocus="on"></input>
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary"></input>
+                        <input 
+                        type="submit" 
+                        value="Search" 
+                        className="btn btn-primary w-100"></input>
                     </div>
                 </div>
             </form>
@@ -21,10 +28,15 @@ export default function Weather() {
                 <li>Wednesday 07:00</li>
                 <li>Mostly Cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row nt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="Mostly Cloudy" />
-                    6 C
+                    <div className="float-left">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="Mostly Cloudy" />
+                        <div className="float-right">
+                            <span className="temperature">6</span>
+                            <span className="unit">°C</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
