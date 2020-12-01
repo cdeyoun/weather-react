24de1978ef4642413c1d3cff5d370bd3ec397f9e
cdeyoun <69175805+cdeyoun@users.noreply.github.com>
2020-12-01 00:20:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York"/>
         <footer>
-          This project was coded by <a href="https://github.com/cdeyoun/weather-react" target="_blank">Cassie DeYoung</a> and is <a href="https://www.linkedin.com/in/cassandradeyoung/" target="_blank">open-sourced on GitHub.</a>
+          This project was coded by <a href="https://github.com/cdeyoun/weather-react" rel="noreferrer" target="_blank" >Cassie DeYoung</a> and is <a href="https://www.linkedin.com/in/cassandradeyoung/" rel="noreferrer" target="_blank" >open-sourced on GitHub.</a>
         </footer>
       </div>
     </div>
